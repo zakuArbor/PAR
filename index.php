1601b6cde7adfc88d72934429e7ce5bcb4e8dacf
zakuArbor <nabimoon1234@gmail.com>
2016-05-02 17:55:53 -0400

    index.php

--- index.php
+++ index.php
@@ -5,7 +5,29 @@
 <head>
 	<title>Title Page</title>
 	<!-- css annd javascript files -->
+	<link rel="stylesheet" type="text/css" href="styles.css">
 </head>
+	<div id = "container">
+		<div id = "header">
+			<div id = "logo"></div>
+			<div id = "program"></div>
+			<div id = "user"></div>
+			<div id = "setting"></div>
+		</div>
+
+		<div id = "nav">
+			
+		</div>
+
+		<div id = "content">
+			
+		</div>
+
+		<div id = "footer">
+			
+		</div>
+
+	</div>
 <body>
 </body>
 </html>

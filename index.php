37dfe6a38456f42e95d800a182c9a3645d207027
zakuArbor <nabimoon1234@gmail.com>
2016-06-30 22:24:05 -0400

    basic design completion

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 #access checker
 ?>
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Title Page</title>
@@ -9,23 +10,58 @@
 </head>
 	<div id = "container">
 		<div id = "header">
-			<div id = "logo"></div>
-			<div id = "program"></div>
-			<div id = "user"></div>
-			<div id = "setting"></div>
+			<ul id = "header">
+			<li class = "logo"><img src="images/logo2.png"></li>
+			<li class = "program"><div id = "main_program">PAR</div><div id = "sub_program">PROGRESS AND REPORTS</div></li>
+			<!--<li class = "user"><img src="images/silhouette.png"></li>
+			<li class = "setting"></li>-->
+			</ul>
 		</div>
 
 		<div id = "nav">
-			
+			<ul id = "nav">
+				<li class = "nav_link">
+					<div class = "nav_header">1</div>
+					<div class = "nav_drop">
+						<a href = "">Link 1</a>		
+						<a href = "">Link 2</a>	
+					</div>
+					
+				</li>
+				<li class = "nav_link"><div class = "nav_header">2</div></li>
+				<li class = "nav_link">
+					<div class = "nav_header">3</div>
+						<div class = "nav_drop">
+							<a href = "">Link 1</a>		
+							<a href = "">Link 2</a>	
+						</div>
+					</li>
+
+				<li class = "nav_icons">
+					<div id = "search"><img src = "images/svg/search.svg"></div>
+					<div id = "report"><img src = "images/svg/report.svg"></div>
+					<div id = "user"><img src = "images/svg/profile.svg"></div>
+					<div id = "setting"><img src = "images/svg/setting.svg"></div>
+					<div id = "power"><img src = "images/svg/power.svg"></div>
+
+				</li>
+			<!--<li class = "user"><img src="images/silhouette.png"></li>
+			<li class = "setting"></li>-->
+			</ul>
 		</div>
 
 		<div id = "content">
-			
-		</div>
+			<p>http://www.flaticon.com/authors/madebyoliver   -power</p>
+			<p>http://www.flaticon.com/authors/freepik        -search</p>
+			<p>http://www.flaticon.com/authors/lucy-g         -profile</p>
+			<p>http://www.flaticon.com/authors/amit-jakhu     -setting</p>
+			<p>http://www.flaticon.com/authors/simpleicon     -report</p>
+			<p>Icon made by [author link] from www.flaticon.com</p>
 
-		<div id = "footer">
-			
+
 		</div>
+		<footer>test<p>d<p>d<p>d<p>d<p>d<p>d<p>d<p><p><p><p><p><p><p><p><p><p><p><p><p><p><p><p><p>d<p><p>		</footer>
+		
 
 	</div>
 <body>

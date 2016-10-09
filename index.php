3dfcd096b20aa1aa73212aa044201a6b556edb36
Ju Hong Kim <nabimoon1234@gmail.com>
2016-10-09 16:36:15 -0400

    Changed URL for jQuery
    Changed URL for jQuery to avoid mixed content error to see if that's the reason why jQuery is not working

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if ($permission_status == 1) {
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=0.4, maximum-scale=1.0, user-scalable=no" />
 		<link rel="stylesheet" type="text/css" href="/HomePage.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js">
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js">
 		<script type="text/javascript" src="/javascript/optionsTab.js"></script>
 		
 		<?php include ($_SERVER['DOCUMENT_ROOT'] . "/javascript/color.php"); ?>
